be68a13e4c6b1fc0952ab93f3d917f59d5711f12
Merajhosen <merajakhon7@gmail.com>
2022-02-13 19:59:50 +0000

    shoping cart

---
+++ app.js
@@ -0,0 +1,55 @@
+function upadateCaseNumber(product, price, isIncreasing) {
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
+    if (isIncreasing) {
+        productNumber = parseInt(productNumber) + 1;
+    }
+    else if (productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1;
+    }
+    productInput.value = productNumber;
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
+
+    // total calculation
+    calculateTotal();
+
+}
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+function calculateTotal() {
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const totalPrice = subTotal + tax;
+    //update on the  html
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-price').innerText = totalPrice;
+}
+//phone increase and decrease event
+
+document.getElementById('phone-plus').addEventListener('click', function () {
+    upadateCaseNumber('phone', 1219, true);
+});
+document.getElementById('phone-minus').addEventListener('click', function () {
+    upadateCaseNumber('phone', 1219, false);
+})
+
+//handle case increase and decrese event
+document.getElementById('case-plus').addEventListener('click', function () {
+    /* const caseInput = document.getElementById('case-number');
+    const caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1; */
+    upadateCaseNumber('case', 59, true);
+});
+document.getElementById('case-minus').addEventListener('click', function () {
+    /* const caseInput = document.getElementById('case-number');
+    const caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) - 1; */
+    upadateCaseNumber('case', 59, false);
+})
